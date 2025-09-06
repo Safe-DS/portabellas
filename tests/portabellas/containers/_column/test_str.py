880c5646698698f2826fc3753d95066ed0633c19
Lars Reimann <mail@larsreimann.com>
2025-09-06 18:18:09 +0200

    feat: str/repr for Column

---
+++ tests/portabellas/containers/_column/test_str.py
@@ -0,0 +1,41 @@
+import pytest
+
+from portabellas import Column
+
+
+@pytest.mark.parametrize(
+    ("column", "expected"),
+    [
+        pytest.param(
+            Column("col1", []),
+            """
+
++------+
+| col1 |
+| ---  |
+| null |
++======+
++------+
+
+            """.strip(),
+            id="empty",
+        ),
+        pytest.param(
+            Column("col1", [0]),
+            """
+
++------+
+| col1 |
+|  --- |
+|  i64 |
++======+
+|    0 |
++------+
+
+            """.strip(),
+            id="non-empty",
+        ),
+    ],
+)
+def test_should_return_a_string_representation(column: Column, expected: str) -> None:
+    assert str(column) == expected
