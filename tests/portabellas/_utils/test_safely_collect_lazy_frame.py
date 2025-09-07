0e8545479e0180ec08c87ad953e175ac8bebf47b
Lars Reimann <mail@larsreimann.com>
2025-09-07 10:46:52 +0200

    test: add missing tests

---
+++ tests/portabellas/_utils/test_safely_collect_lazy_frame.py
@@ -0,0 +1,11 @@
+import polars as pl
+import pytest
+
+from portabellas._utils import safely_collect_lazy_frame
+from portabellas.exceptions import LazyComputationError
+
+
+def test_should_raise_custom_error() -> None:
+    frame = pl.LazyFrame().select("a")
+    with pytest.raises(LazyComputationError):
+        safely_collect_lazy_frame(frame)
